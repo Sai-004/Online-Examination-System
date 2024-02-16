34b1297580cda62f09b95a6c629f4e07510c5cb8
Sai-004 <94926227+Sai-004@users.noreply.github.com>
2024-02-16 16:28:45 +0530

    Exam Panel Design

---
+++ Online Examination System/Online Examination System/ExamForm.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SubmitBtn = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SaveBtn = New System.Windows.Forms.Button()
+        Me.ReviewBtn = New System.Windows.Forms.Button()
+        Me.PrevBtn = New System.Windows.Forms.Button()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(29, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(781, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'SubmitBtn
+        '
+        Me.SubmitBtn.Location = New System.Drawing.Point(684, 439)
+        Me.SubmitBtn.Name = "SubmitBtn"
+        Me.SubmitBtn.Size = New System.Drawing.Size(127, 33)
+        Me.SubmitBtn.TabIndex = 3
+        Me.SubmitBtn.Text = "Submit"
+        Me.SubmitBtn.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(0, -3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1144, 26)
+        Me.Panel1.TabIndex = 3
+        '
+        'SaveBtn
+        '
+        Me.SaveBtn.Location = New System.Drawing.Point(354, 439)
+        Me.SaveBtn.Name = "SaveBtn"
+        Me.SaveBtn.Size = New System.Drawing.Size(127, 33)
+        Me.SaveBtn.TabIndex = 2
+        Me.SaveBtn.Text = "Save and Next"
+        Me.SaveBtn.UseVisualStyleBackColor = True
+        '
+        'ReviewBtn
+        '
+        Me.ReviewBtn.Location = New System.Drawing.Point(192, 439)
+        Me.ReviewBtn.Name = "ReviewBtn"
+        Me.ReviewBtn.Size = New System.Drawing.Size(127, 33)
+        Me.ReviewBtn.TabIndex = 1
+        Me.ReviewBtn.Text = "Mark for review"
+        Me.ReviewBtn.UseVisualStyleBackColor = True
+        '
+        'PrevBtn
+        '
+        Me.PrevBtn.Location = New System.Drawing.Point(33, 439)
+        Me.PrevBtn.Name = "PrevBtn"
+        Me.PrevBtn.Size = New System.Drawing.Size(127, 33)
+        Me.PrevBtn.TabIndex = 0
+        Me.PrevBtn.Text = "Previous"
+        Me.PrevBtn.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 61)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SubmitBtn)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SaveBtn)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ReviewBtn)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.PrevBtn)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1144, 499)
+        Me.SplitContainer1.SplitterDistance = 305
+        Me.SplitContainer1.TabIndex = 5
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(0, 29)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1144, 26)
+        Me.Panel2.TabIndex = 4
+        '
+        'ExamPanel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1143, 557)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Name = "ExamPanel"
+        Me.Text = "Exam Panel"
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents SubmitBtn As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents SaveBtn As System.Windows.Forms.Button
+    Friend WithEvents ReviewBtn As System.Windows.Forms.Button
+    Friend WithEvents PrevBtn As System.Windows.Forms.Button
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+
+End Class
---
+++ Online Examination System/Online Examination System/ExamForm.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Windows.Forms
+
+Public Class Form1
+    ' Define the list of tuples for section names and number of questions
+    Private sectionData As List(Of Tuple(Of String, Integer)) = New List(Of Tuple(Of String, Integer)) From {
+        Tuple.Create("Section 1", 4),
+        Tuple.Create("Section 2", 8),
+        Tuple.Create("Section 3", 4)
+    }
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        LoadSections()
+    End Sub
+
+    Private Sub LoadSections()
+        ' Iterate through the list of tuples in reverse order and create labels for sections and question buttons
+        For i As Integer = sectionData.Count - 1 To 0 Step -1
+            Dim sectionLabel As New Label()
+            sectionLabel.Text = sectionData(i).Item1 ' Item1 is the section name
+            sectionLabel.AutoSize = True
+            sectionLabel.Dock = DockStyle.Top
+            sectionLabel.Margin = New Padding(5)
+            SplitContainer1.Panel1.Controls.Add(sectionLabel)
+
+            Dim questionCount As Integer = sectionData(i).Item2 ' Item2 is the number of questions
+
+            ' Add question buttons for each section
+            Dim rowCount As Integer = Math.Ceiling(questionCount / 4)
+            Dim questionPanel As New TableLayoutPanel()
+            questionPanel.Dock = DockStyle.Top
+            questionPanel.AutoSize = True
+            questionPanel.ColumnCount = 4
+            questionPanel.RowCount = rowCount
+
+            For j As Integer = 1 To questionCount
+                Dim questionButton As New Button()
+                questionButton.Text = "S " & (i + 1) & ", Q " & j
+                questionButton.AutoSize = True
+                questionButton.Margin = New Padding(5)
+                AddHandler questionButton.Click, AddressOf QuestionButton_Click
+
+                Dim rowIndex As Integer = (j - 1) \ 4
+                Dim columnIndex As Integer = (j - 1) Mod 4
+                questionPanel.Controls.Add(questionButton, columnIndex, rowIndex)
+            Next
+
+            ' Add question panel to the SplitContainer1.Panel1.Controls
+            SplitContainer1.Panel1.Controls.Add(questionPanel)
+        Next
+    End Sub
+
+    Private Sub QuestionButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+        ' Handle click event of question buttons
+        Dim questionButton As Button = DirectCast(sender, Button)
+        Dim questionInfo As String = questionButton.Text
+        MessageBox.Show("You clicked on: " & questionInfo)
+
+        ' Here you can change the question in the right panel
+        ' For now, let's just change the text in a textbox
+        TextBox1.Text = "You clicked on: " & questionInfo & ". Question details will be displayed here."
+    End Sub
+
+    ' Add other button click event handlers for previous, clear, save_next, submit
+    ' For example:
+    Private Sub PreviousButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+        ' Handle click event of the previous button
+        ' Add your logic here
+    End Sub
+End Class
