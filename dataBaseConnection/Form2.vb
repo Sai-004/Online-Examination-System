c18e1af86e5754258c1ed1e2b4855e053c7a2b88
Koushik2824 <95124356+Koushik2824@users.noreply.github.com>
2024-02-18 11:35:37 +0530

    Added more admin pages
    Form2 should open if admin entered not for the first time
    Form3 is for question pool management(add,edit,delete questions)

---
+++ dataBaseConnection/Form2.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.Odbc
+'This form opens if admin is entering the admin section not first time
+Public Class Form2
+    Dim connString As String = "DSN=oee;Uid=root;Pwd=2004;"
+    Dim conn As New OdbcConnection(connString)
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Load the form for question pool management here
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'Load the form of student results here
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        'Check if there are any students whose results were not released yet
+        'If No such student exists show the same in message box
+        'If the results were released,show the same in message box
+    End Sub
+End Class
---
+++ dataBaseConnection/Form3.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.Odbc
+Imports System.Windows.Forms
+Imports System.Drawing.Drawing2D
+'This form does question pool management like add,edit and delete
+Public Class Form3
+    Dim connString As String = "DSN=oee;Uid=root;Pwd=2004;"
+    Dim conn As New OdbcConnection(connString)
+    Dim numberOfSections As Integer
+    Dim numberOfQuestions As New List(Of Integer)
+    Private Sub Form3_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            'to fill the variables numberOfSections and numberOfQuestions from database
+            conn.Open()
+            Dim query As String = "SELECT sections, questions FROM numberOfQuestions"
+            Dim cmd As New OdbcCommand(query, conn)
+            ' Execute the query
+            Dim reader As OdbcDataReader = cmd.ExecuteReader()
+            ' Check if there is a row
+            If reader.Read() Then
+                ' Retrieve sections into numberOfSections variable
+                numberOfSections = reader.GetInt32(0)
+                ' Retrieve questions into a string
+                Dim questionsString As String = reader.GetString(1)
+                ' Parse the string of integers into a list of integers
+                Dim integerStrings() As String = questionsString.Split(","c)
+                For Each intString As String In integerStrings
+                    Dim intValue As Integer
+                    If Integer.TryParse(intString, intValue) Then
+                        numberOfQuestions.Add(intValue)
+                    Else
+                        MessageBox.Show("Error parsing the string of number of questions", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                Next
+                ' Now you have the numberOfSections and listOfIntegers
+                MessageBox.Show("numberOfSections: " & numberOfSections.ToString() & vbCrLf &
+                "numberOfQuestions: " & String.Join(",", numberOfQuestions))
+            Else
+                MessageBox.Show("No data retrieved.")
+            End If
+        Catch ex As Exception
+
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+End Class
