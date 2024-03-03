61ab8cecac5dcebbab345958426873acdd29d0f7
Koushik2824 <95124356+Koushik2824@users.noreply.github.com>
2024-03-03 21:16:13 +0530

    validating email and logged_in changing

--- Online Examination System/Online Examination System/LandingPage.vb
+++ Online Examination System/Online Examination System/LandingPage.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.Odbc
 Imports System.Windows.Forms
 Imports System.Drawing.Drawing2D
+Imports System.Text.RegularExpressions
 
 Public Class LandingPage
     Dim connString As String = "DSN=oee;Uid=user123;Pwd=1234"
@@ -103,7 +104,6 @@ Public Class LandingPage
     Private Sub SaveUserDetails(ByVal name As String, ByVal rollNumber As String, ByVal email As String, ByVal password As String)
         Dim query As String = ""
         query = "INSERT INTO student (name, roll_number, email, password) VALUES (?, ?, ?, ?)"
-
         Try
             connection.Open()
             Using cmd As New OdbcCommand(query, connection)
@@ -119,7 +119,13 @@ Public Class LandingPage
             connection.Close()
         End Try
     End Sub
-
+    'to validate the email
+    Private Function ValidateEmail(ByVal email As String) As Boolean
+        Dim pattern As String = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        Dim regex As New Regex(pattern)
+        Dim isMatch As Boolean = regex.IsMatch(email)
+        Return isMatch
+    End Function
     Private Sub SignIn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SignInBtn.Click
         Dim Name As String
         Dim RollNumber As String
@@ -130,27 +136,83 @@ Public Class LandingPage
         If SignInBtn.Text = "Login" Then 'admin login
             email = Textbox2.Text
             Password = Textbox3.Text
-
+            Dim validEmail As Boolean = ValidateEmail(email)
             userExists = ValidateUserDetails(email, Password, "Admin")
-            If userExists = True Then
-                MessageBox.Show("Admin Validated!")
+            If userExists = True And validEmail = True Then
+                MessageBox.Show("Admin Validated!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                'to check if the admin entered for the first time or not
+                'Try
+                '    Dim entered As Boolean = False
+                '    connection.Open()
+                '    Dim query As String = "select entered from admin"
+                '    Using cmd As New OdbcCommand(query, connection)
+                '        Dim reader As OdbcDataReader = cmd.ExecuteReader()
+                '        If reader.Read() Then
+                '            entered = Convert.ToBoolean(reader("entered"))
+                '        End If
+                '    End Using
+                '    If entered = True Then
+                '        Dim otherOpen As New otherOpen()
+                '        otherOpen.Show()
+                '        Me.Hide()
+                '    Else
+                '        'update the entry
+                '        query = "update admin set entered=?"
+                '        Using cmd As New OdbcCommand(query, connection)
+                '            cmd.Parameters.AddWithValue("?", 1)
+                '            cmd.ExecuteNonQuery()
+                '        End Using
+                '        Dim firstOpen As New firstOpen()
+                '        firstOpen.Show()
+                '        Me.Hide()
+                '    End If
+                'Catch ex As Exception
+                'Finally
+                '    connection.Close()
+                'End Try
             Else
-                MessageBox.Show("Error: Invalid Credentials")
+                MessageBox.Show("Invalid Credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
 
         ElseIf SignInBtn.Text = "Sign In" Then 'student login
             RollNumber = Textbox2.Text
             Password = Textbox3.Text
             ValidateUserDetails(RollNumber, Password, "Student")
-
             userExists = ValidateUserDetails(RollNumber, Password, "Student")
             If userExists = True Then
                 ' MessageBox.Show("Student Validated!")
+                Dim intRoll As Integer
+                If Not Integer.TryParse(RollNumber.Trim(), intRoll) Then
+                    MessageBox.Show("Invalid input for roll number.")
+                    Exit Sub
+                End If
+                Try
+                    Dim logged_in As Boolean
+                    connection.Open()
+                    Dim query As String = "select logged_in from student where roll_number=?"
+                    Using cmd As New OdbcCommand(query, connection)
+                        cmd.Parameters.AddWithValue("?", intRoll)
+                        Dim reader As OdbcDataReader = cmd.ExecuteReader()
+                        If reader.Read() Then
+                            logged_in = Convert.ToBoolean(reader("logged_in"))
+                        End If
+                    End Using
+                    If logged_in Then
+                        MessageBox.Show("You are already logged in,please exit from the other application", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Application.Exit()
+                    Else
+                        logged_in = True
+                    End If
+                Catch ex As Exception
+
+                Finally
+                    connection.Close()
+                End Try
                 Dim stuLandPage As New StudentLandingPage(RollNumber)
                 stuLandPage.Show()
                 Me.WindowState = FormWindowState.Minimized
             Else
-                MessageBox.Show("Student Error: Invalid Credentials")
+                MessageBox.Show("Invalid Credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
 
         ElseIf SignInBtn.Text = "Sign Up" Then 'student registration
@@ -158,7 +220,11 @@ Public Class LandingPage
             RollNumber = Textbox2.Text
             email = Textbox3.Text
             Password = Textbox4.Text
-
+            Dim validEmail As Boolean = ValidateEmail(email)
+            If validEmail = False Then
+                MessageBox.Show("Invalid Email", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Application.Exit()
+            End If
             ' Save user details into the database
             SaveUserDetails(Name, RollNumber, email, Password)
             ' MessageBox.Show("Student added successfully")
@@ -178,4 +244,5 @@ Public Class LandingPage
     Private Sub Panel2_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel2.Paint
         Panel1.BackColor = Color.FromArgb(200, 255, 255, 255)
     End Sub
+
 End Class
